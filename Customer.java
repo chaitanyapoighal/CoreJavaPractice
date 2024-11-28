073bbbcf37b537858fc0a7c93d8d8c5c7d61bffd
chaitanyapoighal <poighalchaitanya@gmail.com>
2024-11-28 22:16:07 +0530

    first commit

---
+++ Customer.java
@@ -0,0 +1,10 @@
+public class Customer{
+    String name;
+    int age;
+	public static void main(String args[])
+	{
+  	  Customer c1=new Customer();
+  	  System.out.println(c1.name);
+  	  System.out.println(c1.age);
+	}
+}
